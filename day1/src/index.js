4131194973180467f47930da8050ad1b47145273
David Lozzi <david@lozzi.net>
2022-12-01 16:19:12 -0500

    tshooting router

--- day1/src/index.js
+++ day1/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router basename={process.env.BASEURL}>
       <Routes>
-        <Route path='/' element={<App />} />
+        <Route path='/' element={<App />} exact />
       </Routes>
     </Router>
   </React.StrictMode>
